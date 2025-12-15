3e3b0d4f0d6c228e92cd731787cb201246fb234c
ayman blidi <blidiayman47@gmail.com>
2025-12-15 10:06:55 +0100

    Tour models/serializers

---
+++ backend/dztour/tours/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Tour, TourPicture
+from ..guides.models import GuideProfile
+from ..guides.serializers import GuideProfileSerializer
+
+class TourPictureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TourPicture
+        fields = '__all__'
+
+class TourSerializer(serializers.ModelSerializer):
+    guide = GuideProfileSerializer(read_only=True)
+    pictures = TourPictureSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Tour
+        fields = '__all__'
+        read_only_fields = ['guide', 'price']
