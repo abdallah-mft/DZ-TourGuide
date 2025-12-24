e7923107666dc9a52598e6348f72b55591014c1a
ayman blidi <blidiayman47@gmail.com>
2025-12-24 18:37:23 +0100

    delete tour image endpoint

--- backend/dztour/tours/views.py
+++ backend/dztour/tours/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, permissions, status, filters
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
 
 from .models import Tour, TourPicture
 from .serializers import TourSerializer, TourPictureSerializer
@@ -38,6 +39,19 @@ class TourViewSet(viewsets.ModelViewSet):
         serializer.save(guide=self.request.user.guide)  
 
 
+    @action(detail=False, methods=['GET'], url_path='for-guide')
+    def list_guide_tours(self, request):
+        guide = request.user.profile
+        tours = Tour.objects.filter(guide=guide)
+        serializer = TourSerializer(tours, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['DELETE'], url_path='delete-image/(?P<image_id>[^/.]+)')
+    def delete_image(self, request, pk=None, image_id=None):
+        image = get_object_or_404(TourPicture, id=image_id)
+        image.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     @action(detail=True, methods=['POST'], url_path='add-images')
     def add_images(self, request, pk=None):
         tour = self.get_object()
@@ -63,10 +77,4 @@ class TourViewSet(viewsets.ModelViewSet):
                 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-    @action(detail=False, methods=['GET'], url_path='for-guide')
-    def list_guide_tours(self, request):
-        guide = request.user.profile
-        tours = Tour.objects.filter(guide=guide)
-        serializer = TourSerializer(tours, many=True)
-        return Response(serializer.data)
+    
